0824d07813f9258550a0427a2b68a99f7dda9629
Nicolas De Boose <nikoms@gmail.com>
2016-10-29 21:13:41 +0200

    Do not output on bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,21 +1,20 @@
 <?php
 include(__DIR__.'/../vendor/autoload.php');
 use Nikoms\PhpUnitSplitter\TestCase\SplitStep;
-use Nikoms\PhpUnitSplitter\TestCase\Token;
 
 if (SplitStep::isSplitting()) {
-    echo sprintf('Bootstrap of splitting (%s jobs)', SplitStep::getValue()).PHP_EOL;
+//    echo sprintf('Bootstrap of splitting (%s jobs)', SplitStep::getValue()).PHP_EOL;
 }
 
 if (SplitStep::isRunning()) {
-    echo sprintf(
-            'Bootstrap of run. Token: %s. Running group:',
-            Token::getTestToken(),
-            Token::getRunningGroup()
-        )
-        .PHP_EOL;
+//    echo sprintf(
+//            'Bootstrap of run. Token: %s. Running group:',
+//            Nikoms\PhpUnitSplitter\TestCase\Token::getTestToken(),
+//            Nikoms\PhpUnitSplitter\TestCase\Token::getRunningGroup()
+//        )
+//        .PHP_EOL;
 }
 
 if (SplitStep::isGathering()) {
-    echo sprintf('Bootstrap of gathering (%s tests)', SplitStep::getValue()).PHP_EOL;
+//    echo sprintf('Bootstrap of gathering (%s tests)', SplitStep::getValue()).PHP_EOL;
 }
